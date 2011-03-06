16add545c8b30465e5601c6b93220180d73d87d2
Dr. ERDI Gergo <gergo@erdi.hu>
2011-03-06 22:09:15 +0100

    Reconstruct parentheses and infix operators

--- src/Vis.hs
+++ src/Vis.hs
@@ -10,6 +10,7 @@ import Vis.Reduce
 
 import qualified Language.Haskell.Syntax as H
 import Language.Haskell.Parser
+import Language.Haskell.Infix
 import Language.Haskell.Pretty
 import Control.Applicative
 import Control.Monad
@@ -42,22 +43,26 @@ test = do
              "take _ [] = []",
              "take n (x:xs) = x:take (n-1) xs",
              "",
-             -- "main = take (length' [1,2,3,4,5]) ones",
+             "main = take (length [1,2,3,4,5]) ones",
              -- "main = let xy = steppers 3",
              -- "           inc = fst xy",
              -- "           dec = snd xy",
              -- "       in dec 4",
-             "main = let nats = 1:map (\\x -> x + 1) nats in nats",
+             -- "main = let nats = 1:map (\\x -> x + 1) nats in nats",
              ""
              ]  
       ParseOk mod = parseModule src
-      H.HsModule _ _ _ _ decls = mod  
+      H.HsModule _ _ _ _ decls' = mod  
+      decls = infixer emptyInfixMap decls'
   withDecls decls $ do
     forM_ decls $ \decl -> do
       (x, node) <- fromDecl decl      
       setVar x node
     Just main <- lookupBind (Name $ H.HsIdent "main")
-    -- unlines <$> replicateM 1 (reduce main >> (liftM prettyPrint $ liftST (toSource main)))
-    liftM prettyPrint $ liftST (liftM toSource $ flatten main)
+    unlines <$> (replicateM steps $ do      
+      result <- liftM prettyPrint $ liftST (liftM toSource $ flatten main)
+      reduce main
+      return result)
+  where steps = 1
 
 main = putStrLn $ runST $ runVis test
--- src/Vis/ToSource.hs
+++ src/Vis/ToSource.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PatternGuards #-}
 module Vis.ToSource (toSource) where
 
 import Vis.Node
@@ -16,7 +17,38 @@ import qualified Data.Map as Map
 import Data.Monoid
 
 toSource :: FNode -> H.HsExp
-toSource = projectNode
+toSource = parenExpr . projectNode
+
+parenExpr (H.HsApp (H.HsApp op left) right) | Just opName <- getInfix = H.HsInfixApp left' opName right'
+  where getInfix = case op of
+          H.HsCon c@(H.Special H.HsCons) -> Just $ H.HsQConOp c
+          H.HsVar sym@(H.UnQual (H.HsSymbol _)) -> Just $ H.HsQVarOp sym
+          _ -> Nothing
+        
+        forceTopParen e@(H.HsApp _ _) = H.HsParen e
+        forceTopParen e = e
+        
+        left' = forceTopParen $ parenExpr left
+        right' = forceTopParen $ parenExpr right
+          
+parenExpr (H.HsApp e f@(H.HsApp _ _)) = H.HsApp (parenExpr e) (H.HsParen (parenExpr f))
+parenExpr (H.HsApp e f) = H.HsApp (parenExpr e) (parenExpr f)
+parenExpr (H.HsLambda loc pats body) = H.HsLambda loc pats $ parenExpr body
+parenExpr (H.HsLet decls body) = H.HsLet (map parenDecl decls) $ parenExpr body
+parenExpr (H.HsCase e alts) = H.HsCase (parenExpr e) (map parenAlt alts)
+parenExpr expr = expr
+
+parenAlt (H.HsAlt loc pat gAlt wheres) = H.HsAlt loc pat (parenGAlt gAlt) (map parenDecl wheres)
+
+parenGAlt (H.HsUnGuardedAlt expr) = H.HsUnGuardedAlt $ parenExpr expr
+
+parenDecl (H.HsPatBind loc pat rhs wheres) = H.HsPatBind loc pat (parenRhs rhs) (map parenDecl wheres)
+parenDecl (H.HsFunBind matches) = H.HsFunBind $ map parenMatch matches
+
+parenMatch (H.HsMatch loc name pats rhs wheres) = H.HsMatch loc name pats (parenRhs rhs) (map parenDecl wheres)
+
+parenRhs (H.HsUnGuardedRhs expr) = H.HsUnGuardedRhs $ parenExpr expr
+
 
 projectNode :: FNode -> H.HsExp
 projectNode (FNode payload) = projectPayload payload
@@ -49,14 +81,20 @@ projectPayload Uninitialized = H.HsWildCard
 projectPayload (ParamRef x) = H.HsVar $ projectName x
 projectPayload (IntLit n) = H.HsLit $ H.HsInt n  
 projectPayload (App e f) = H.HsApp (projectNode e) (projectNode f)
-projectPayload (BuiltinFunApp op args) = H.HsParen $ toApp $ fun:(map projectNode args)
+projectPayload (BuiltinFunApp op args) = toApp $ fun:(map projectNode args)
   where fun = case op of
           IntPlus -> H.HsVar (H.UnQual $ H.HsSymbol "+#")
           IntMinus -> H.HsVar (H.UnQual $ H.HsSymbol "-#")
 projectPayload (ConApp c args) = toApp $ con:(map projectNode args)
   where con = H.HsCon $ projectName c
 projectPayload (CaseApp arity [Alt pats body] []) = H.HsParen $ H.HsLambda noLoc pats $ projectNode body
+projectPayload (CaseApp 1 alts args) = toApp $ (H.HsCase expr $ map projectAlt alts):map projectNode args
+  where expr = case args of 
+          [] -> H.HsWildCard
+          [arg] -> projectNode arg        
+        projectAlt (Alt [pat] node) = H.HsAlt noLoc pat (H.HsUnGuardedAlt $ projectNode node) []
 projectPayload (CaseApp arity alts args) = toApp $ (H.HsCase expr $ map projectAlt alts):map projectNode args
+
   where expr = H.HsTuple $ ensureLen arity $ map projectNode args
         projectAlt (Alt pats node) = let body = projectNode node
                                      in H.HsAlt noLoc (H.HsPTuple pats) (H.HsUnGuardedAlt body) []
