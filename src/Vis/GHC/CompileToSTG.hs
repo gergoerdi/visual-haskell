945c046453017a06d4c5c9edbd29564e6bef1a29
Dr. ERDI Gergo <gergo@erdi.hu>
2011-03-21 15:22:00 +0100

    Honor pragmas in source files

--- src/Vis/GHC/CompileToSTG.hs
+++ src/Vis/GHC/CompileToSTG.hs
@@ -20,67 +20,41 @@ import Var (Id)
 import StgSyn (GenStgBinding)
 import TcRnTypes (TcGblEnv)
 
-getModSummary' :: (GhcMonad m) => HscEnv -> FilePath -> m ModSummary
-getModSummary' hsc_env filename = do
-  let dflags = hsc_dflags hsc_env
-      
-  buf <- liftIO $ hGetStringBuffer filename
-  src_timestamp <- liftIO $ getModificationTime filename  
-  
-  (src_imps, imps, L _ mod_name) <- getImports dflags buf filename filename
-  
-  let loc = ModLocation{ ml_hs_file   = Nothing,
-                         ml_hi_file   = error "hi_file",
-                         ml_obj_file  = error "obj_file" }
-  mod <- liftIO $ addHomeModuleToFinder hsc_env mod_name loc
-  
-  return ModSummary {
-    ms_mod = mod,
-    ms_hsc_src = HsSrcFile,
-    ms_hspp_file = filename,
-    ms_hspp_opts = dflags,
-    ms_hspp_buf = Just buf,
-    ms_location = error "ms_location",
-    ms_hs_date = src_timestamp,
-    ms_obj_date = Nothing,
-    ms_imps = [],
-    ms_srcimps = src_imps
-    }
+import System.Environment (getArgs)
 
-parseAndTypecheck :: (GhcMonad m) => ModSummary -> m TcGblEnv
-parseAndTypecheck mod_summary = do 
-  rdr_module <- hscParse mod_summary
-  hscTypecheck mod_summary rdr_module
+parseAndTypecheck :: (GhcMonad m) => ModSummary -> m DesugaredModule
+parseAndTypecheck mod = parseModule mod >>= typecheckModule >>= desugarModule
 
 type Stg id = [(GenStgBinding id id, [(id, [id])])]
 
-compileToStg :: (GhcMonad m) => HscEnv -> ModSummary -> m (Stg Id)
-compileToStg hsc_env mod_summary = do
-  tcr <- parseAndTypecheck mod_summary
-  guts <- hscDesugar mod_summary tcr
+compileToStg :: (GhcMonad m) => ModSummary -> m (Stg Id)
+compileToStg mod = do
+  core <- parseAndTypecheck mod
+  let guts = coreModule core
   guts' <- hscSimplify guts
   (_iface, _changed, _details, cgguts) <- hscNormalIface guts' Nothing
   
   liftIO $ do
-    let dflags = hsc_dflags hsc_env
+    let dflags = ms_hspp_opts mod
         coreBinds = cg_binds cgguts
         tycons = cg_tycons cgguts
         data_tycons = filter isDataTyCon tycons      
         this_mod = cg_module cgguts    
     prepd_binds <- corePrepPgm dflags coreBinds data_tycons
-    stg_binds <- coreToStg (thisPackage dflags) prepd_binds    
+    stg_binds <- coreToStg (modulePackageId this_mod) prepd_binds    
     (stg_binds', _ccs) <- stg2stg dflags this_mod stg_binds
     return stg_binds'
   
 toStg :: FilePath -> IO [Stg Id]
 toStg filename = runGhc (Just libdir) $ do
+  args <- liftIO getArgs
   dflags <- getSessionDynFlags
-  _ <- setSessionDynFlags dflags
+  (dflags', lfilenames, _) <- parseDynamicFlags dflags (map noLoc args)
+  let filenames = map unLoc lfilenames
+  _ <- setSessionDynFlags dflags'
 
-  target <- guessTarget filename Nothing
-  setTargets [target]
+  targets <- mapM (guessTarget `flip` Nothing) filenames
+  setTargets targets
   _ <- load LoadAllTargets
   mods <- depanal [] False
-
-  withSession $ \hsc_env ->
-    mapM (compileToStg hsc_env) mods
+  mapM compileToStg mods
