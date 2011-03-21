3dc3a16daea9b7f67e661c831807d1f593e493bd
Dr. ERDI Gergo <gergo@erdi.hu>
2011-03-21 14:19:03 +0100

    STG -> SimpleSTG

--- src/Vis/GHC/CompileToSTGTest.hs
+++ src/Vis/GHC/CompileToSTGTest.hs
@@ -1,13 +1,22 @@
 module Main where
 
 import Vis.GHC.CompileToSTG
+import Vis.GHC.SimpleSTG
 
 import Outputable (printDump)
 import StgSyn (pprStgBindingsWithSRTs)
 import System.Environment (getArgs)
+import Control.Monad
+
+import Name
 
 main :: IO ()
 main = do    
   (file:_) <- getArgs
-  stg <- toStg file
-  mapM_ (printDump . pprStgBindingsWithSRTs) stg
+  stgs <- toStg file
+  forM_ stgs $ \stg -> do
+    printDump $ pprStgBindingsWithSRTs stg
+    mapM_ print $ map (simplifyBinding . fst) stg
+
+instance (Show Name) where
+  show name = show (occNameString $ nameOccName name) ++ show (nameUnique name)
---
+++ src/Vis/GHC/SimpleSTG.hs
@@ -0,0 +1,82 @@
+module Vis.GHC.SimpleSTG where
+
+import StgSyn
+import CoreSyn (AltCon(..))
+import DataCon
+import Name
+import Literal
+import Type
+
+import Control.Arrow
+-- import Control.Applicative
+-- import Control.Monad.Reader
+-- import Data.Map (Map)
+-- import qualified Data.Map as Map
+-- import Data.Maybe
+import Debug.Trace
+
+data SStgExpr id = SStgApp id [SStgArg id]
+                 | SStgLit Literal
+                 | SStgConApp id [SStgArg id]
+                 -- | SStgOpApp StgOp [SStgArg id] Type -- TODO: GenStgOpApp
+                 | SStgLam [id] (SStgExpr id)
+                 | SStgCase (SStgExpr id) [SStgAlt id]
+                 | SStgLet [SStgBinding id] (SStgExpr id)
+                 deriving Show
+                           
+data SStgArg id = SStgArgVar id
+                | SStgArgLit Literal
+                  -- | SStgArgType Type -- TODO: Type?
+                deriving Show
+
+data SStgAlt id = SStgAlt (SStgAltCon id) (SStgExpr id)
+                deriving Show
+                          
+data SStgAltCon id = SStgAltData id [id]
+                   | SStgAltLit Literal
+                   | SStgAltWildcard
+                   deriving Show
+                      
+data SStgBinding id = SStgBinding id (SStgRhs id)
+                    deriving Show
+                              
+data SStgRhs id = SStgRhsCon id [SStgArg id]
+                | SStgRhsClosure SUpdateFlag [id] (SStgExpr id)
+                deriving Show
+                          
+data SUpdateFlag = SUpdatable | SReEntrant                          
+                 deriving Show
+
+simplifyBinding :: StgBinding  -> [SStgBinding Name]
+simplifyBinding (StgNonRec x body) = [SStgBinding (getName x) (simplifyRhs body)]
+simplifyBinding (StgRec binds) = map (uncurry SStgBinding . (getName *** simplifyRhs)) binds
+
+simplifyRhs :: StgRhs -> SStgRhs Name
+simplifyRhs (StgRhsCon _ con args) = SStgRhsCon (getName con) $ map simplifyArg args
+simplifyRhs (StgRhsClosure _ _ _ update _ args expr) = SStgRhsClosure update' (map getName args) (simplifyExpr expr)
+  where update' = case update of          
+          ReEntrant -> SReEntrant
+          _ -> SUpdatable
+
+simplifyArg :: StgArg -> SStgArg Name
+simplifyArg (StgVarArg x) = SStgArgVar (getName x)
+simplifyArg (StgLitArg lit) = SStgArgLit lit
+simplifyArg (StgTypeArg _) = error "StgTypeArg"
+
+simplifyExpr :: StgExpr -> SStgExpr Name
+simplifyExpr (StgApp f args) = SStgApp (getName f) $ map simplifyArg args
+simplifyExpr (StgLit lit) = SStgLit lit
+simplifyExpr (StgConApp con args) = SStgConApp (getName con) $ map simplifyArg args
+simplifyExpr (StgOpApp op args _) = error "TODO: StgOpApp"
+simplifyExpr (StgLam _ xs body) = SStgLam (map getName xs) $ simplifyExpr body
+simplifyExpr (StgCase e _ _ _ _ _ alts) = SStgCase (simplifyExpr e) $ map simplifyAlt alts
+simplifyExpr (StgLet binding body) = SStgLet (simplifyBinding binding) $ simplifyExpr body
+simplifyExpr (StgLetNoEscape _ _ binding body) = simplifyExpr (StgLet binding body)
+simplifyExpr (StgSCC _ e) = simplifyExpr e
+simplifyExpr (StgTick _ _ e) = simplifyExpr e
+
+simplifyAlt (con, vars, _, e) = SStgAlt con' $ simplifyExpr e
+  where con' = case con of
+          DataAlt con -> SStgAltData (getName con) (map getName vars)
+          LitAlt lit -> SStgAltLit lit
+          DEFAULT -> SStgAltWildcard
